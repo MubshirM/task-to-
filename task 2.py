ea1b23d6d24ebe77207ffed0d9cb4c86efe6ccd3
MubshirM <mubshirmmm@gmail.com>
2023-02-20 07:44:01 -0800

    first commit

---
+++ task 2.py
@@ -0,0 +1,7 @@
+name1 = int(input("Enter the first integer: "))
+name2 = int(input("Enter the second integer: "))
+
+if name1 > name2:
+    print("The larger integer is:", name1)
+else:
+    print("The larger integer is:", name2)
